42936e35c3c365c1dab70fcdbf8c95a4970775d1
rickyzatnika <rickyzatnika91@gmail.com>
2025-03-04 08:13:44 +0700

    mantap jiwa

--- app/components/PathnameDisplay.tsx
+++ app/components/PathnameDisplay.tsx
@@ -1,9 +1,14 @@
-"use client";
 import { usePathname } from "next/navigation";
 
 export function PathnameDisplay() {
-    const pathname = usePathname(); // Akan selalu berubah saat navigasi
-    const lastSegment = pathname.split("/").filter(Boolean).pop(); // Ambil bagian terakhir
+    const pathname = usePathname(); // Mendapatkan pathname
+    const segments = pathname.split("/").filter(Boolean); // Pisahkan segment path
+    let lastSegment = segments.pop(); // Ambil segment terakhir
 
-    return <p className="capitalize font-semibold text-lg ">{lastSegment}</p>; // Kapitalisasi jika perlu
+    // Cek apakah segment terakhir adalah ID (angka atau kombinasi angka & huruf)
+    if (lastSegment && /^[a-zA-Z0-9]{8,}$/.test(lastSegment)) {
+        lastSegment = segments.pop() || ""; // Ambil segment sebelumnya jika ada
+    }
+
+    return <p className="capitalize font-semibold text-lg">{lastSegment}</p>;
 }
